2e9b88cefc01cfbfc5db49bf6385e988ecd8b83d
Katerina Trajchevska <ktrajchevska@gmail.com>
2018-09-27 11:08:09 +0200

    Add dynamic content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,15 @@
 	<meta charset="utf-8">
 	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
-	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
 	
-	<title>Katerina Trajchevska, Senior Software Engineer</title>
+	<title>Katerina Trajchevska, Software Engineer and Entrepreneur</title>
 
 	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
 	
 	<!-- Bootstrap -->
 	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
 	<!-- Icons -->
-	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
 	<!-- Fonts -->
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,700">
 	<!-- Custom styles -->
@@ -24,13 +23,18 @@
 <body class="home">
 
 <header id="header">
-	<div id="head" class="parallax" parallax-speed="2" style="position:relative;z-index:-2">
-		<div style="position: absolute;top:0;bottom:0;height:100%;width:100%;background: rgba(255,255,255,0.1);content:'';z-index: -1;"></div>
+	<div id="head">
 		<h1 id="logo" class="text-center">
 			<img class="img-circle" src="assets/images/profile.jpg" alt="">
 			<span class="title">Katerina Trajchevska</span>
 			<span class="tagline">Software engineer, tech volunteer,<br/> remote work advocate and co-founder of <a target="_blank" href="https://adeva.co">Adeva</a></span>
 		</h1>
+		<p class="social">
+			<a href="https://www.linkedin.com/in/trajchevska/" target="_blank"><i class="fa fa-linkedin fa-2"></i></a>
+			<a href="https://twitter.com/ktrajchevska" target="_blank"><i class="fa fa-twitter fa-2"></i></a>
+			<a href="https://github.com/trajchevska" target="_blank"><i class="fa fa-github fa-2"></i></a>
+			<a href="https://medium.com/@trajchevska" target="_blank" style="font-size: 18px;"><i class="fa fa-medium fa-2"></i></a>
+		</p>
 	</div>
 
 	<nav class="navbar navbar-default navbar-sticky">
@@ -45,7 +49,7 @@
 				<ul class="nav navbar-nav">
 					<li><a href="#about">About</a></li>
 					<li><a href="#projects">Projects &amp; Initiatives</a></li>
-					<li><a href="#speeches">Speeches</a></li>
+					<li><a href="#speeches">Conferences &amp; Hackathons</a></li>
 					<li><a href="#insights">Insights</a></li>
 				</ul>
 			
@@ -60,8 +64,8 @@
 		
 		<div class="row section topspace" id="about">
 			<div class="col-md-12">
-				<p class="lead text-center text-muted">Hey, there! 👋 I am Katerina, software engineer and entrepreneur.</p> 
-				<p class="lead text-center text-muted">Leading <a href="https://adevait.com">Adeva</a> for over {{$adevaLeadYears}} years, I've had the chance to collaborate with various companies from early stage startups to Fortune 500. That helped me gain valuable insights in many different industries, work with passionate teams and innovate.</p>
+				<p class="lead text-center text-muted">Hey, there! 👋 I am Katerina, software engineer and co-founder of <a href="https://adevait.com">Adeva</a>.</p> 
+				<p class="lead text-center text-muted">Leading Adeva for over {{$adevaLeadYears}} years, I've had the chance to collaborate with various companies from early stage startups to Fortune 500. That helped me gain valuable insights in many different industries, work with passionate teams and innovate.</p>
 
 				<p class="lead text-center text-muted"></p>
 			</div>
@@ -70,8 +74,11 @@
 		<div class="row section recentworks topspace" id="projects">
 			
 			<h2 class="section-title"><span>Projects &amp; Initiatives</span></h2>
+			<div class="col-md-10 col-md-offset-1 section-intro">
 
-			<p class="text-center">I stand for equality, inclusion, and giving back to the community, both personally and professionally. Actively take part of initiatives for women in tech, contribute to the local tech community, and volunteer as a mentor in different programs and initiatives.</p>
+				<p class="text-center">I stand for equality, inclusion, and giving back to the community, both personally and professionally. In my free time, I like to take part of women in tech movements, contribute to the local tech community, and volunteer as a mentor in different programs.</p>
+				<p class="text-center">Some projects and initiatives I'm currently part of 👇</p>
+			</div>
 			
 			<div class="thumbnails recentworks row">
 				
@@ -85,14 +92,13 @@
 						<span class="title">{{$project['name']}}</span>
 					</a>
 					<p class="details">{{$project['description']}}</p>
+					<p class="details"><b>Involved as:</b> {{$project['involvement']}}</p>
 					<span class="details">
 						@foreach($project['tags'] as $key => $tag)
 						@if($key != 0) | @endif
-						<a href="">{{$tag}}</a>
+						<a>{{$tag}}</a>
 						@endforeach
 					</span>
-					<h4></h4>
-					<p></p>
 				</div>
 				@endforeach
 
@@ -101,34 +107,27 @@
 		</div>
 		
 		<div class="row section featured topspace" id="speeches">
-			<h2 class="section-title"><span>Services</span></h2>
+			<h2 class="section-title"><span>Conferences &amp; Hackathons</span></h2>
+			<div class="col-md-10 col-md-offset-1 section-intro">
+				<p class="text-center">I am a tech enthusiast and I love sharing knowledge. And, I strongly believe in the cause of using technology for a greater good so I try to apply it wherever possible. Right now, that is by volunteering with workshops and speeches on conferences, as well as mentoring at various hackathons locally. Here are the ones I'm most proud of.</p>
+			</div>
 			<div class="row">
+				@foreach($conferences as $conference)
 				<div class="col-sm-6 col-md-3">
-					<h3 class="text-center">Custom website design</h3>
-					<p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
-					<p class="text-center"><a href="" class="btn btn-action">Read more</a></p>
+					<h3 class="text-center">{{$conference['name']}}</h3>
+					<p>{{$conference['description']}}</p>
+					<p class="text-center"><a href="{{$conference['href']}}" target="_blank" class="btn btn-action">Read more</a></p>
 				</div>
-				<div class="col-sm-6 col-md-3">
-					<h3 class="text-center">Wordpress integration</h3>
-					<p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
-					<p class="text-center"><a href="" class="btn btn-action">Read more</a></p>
-				</div>
-				<div class="col-sm-6 col-md-3">
-					<h3 class="text-center">Application development</h3>
-					<p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
-					<p class="text-center"><a href="" class="btn btn-action">Read more</a></p>
-				</div>
-				<div class="col-sm-6 col-md-3">
-					<h3 class="text-center">SEO &amp; SEM services</h3>
-					<p>I don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. i did the same thing to gandhi, he didn't eat for three weeks. i once heard a wise man say there are no perfect men.</p>
-					<p class="text-center"><a href="" class="btn btn-action">Read more</a></p>
-				</div>
+				@endforeach
 			</div>
 		</div> <!-- / section -->
 	
 		<div class="row section recentworks topspace" id="insights">
 			
 			<h2 class="section-title"><span>Insights</span></h2>
+			<div class="col-md-10 col-md-offset-1 section-intro">
+				<p class="text-center">I am passionate about technology, remote work, and leadership - and I often like to share my opinions on those 👀</p>
+			</div>
 			
 			<div class="thumbnails recentworks row">
 				@foreach($insights as $insight)
@@ -158,92 +157,26 @@
 		<div class="row section topspace" id="contact">
 			<div class="panel panel-cta"><div class="panel-body">
 				<div class="col-lg-8">
-					<p>A simple, nice-looking <b>call to action box</b>. Boxing is about respect. getting it for yourself, 
-					and taking it away from the other guy. no, this is mount everest. </p>
+					<p>Do you need advice on software or remote work? Would you like me to speak or mentor at your event? Or, you just want to get in touch?</p>
 				</div>
 				<div class="col-lg-4 text-right">
-					<a href="http://www.gettemplate.com/downloads/initio.zip " class="btn btn-primary btn-lg">Download template</a>
+					<a href="mailto:trajchevska@adeva.co" class="btn btn-primary btn-lg">Say hi</a>
 				</div>
 			</div></div>
-		</div> <!-- /section -->
-
-		<div class="row section clients topspace">
-			<h2 class="section-title"><span>Clients</span></h2>
-			<div class="col-lg-12">
-				<p>
-					<img src="assets/images/sample_clients.png" alt="">
-				</p>
-			</div>
 		</div> <!-- /section -->
 
 	</div>	<!-- /container -->
 
 </main>
 
-<footer id="footer">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-3 widget">
-				<h3 class="widget-title">Contact</h3>
-				<div class="widget-body">
-					<p>+234 23 9873237<br>
-						<a href="mailto:#">some.email@somewhere.com</a><br>
-						<br>
-						234 Hidden Pond Road, Ashland City, TN 37015
-					</p>	
-				</div>
-			</div>
-
-			<div class="col-md-3 widget">
-				<h3 class="widget-title">Follow me</h3>
-				<div class="widget-body">
-					<p class="follow-me-icons">
-						<a href=""><i class="fa fa-twitter fa-2"></i></a>
-						<a href=""><i class="fa fa-dribbble fa-2"></i></a>
-						<a href=""><i class="fa fa-github fa-2"></i></a>
-						<a href=""><i class="fa fa-facebook fa-2"></i></a>
-					</p>
-				</div>
-			</div>
-
-			<div class="col-md-3 widget">
-				<h3 class="widget-title">Text widget</h3>
-				<div class="widget-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, nihil natus explicabo ipsum quia iste aliquid repellat eveniet velit ipsa sunt libero sed aperiam id soluta officia asperiores adipisci maxime!</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, nihil natus explicabo ipsum quia iste aliquid repellat eveniet velit ipsa sunt libero sed aperiam id soluta officia asperiores adipisci maxime!</p>
-				</div>
-			</div>
-
-			<div class="col-md-3 widget">
-				<h3 class="widget-title">Form widget</h3>
-				<div class="widget-body">
-					<p>+234 23 9873237<br>
-						<a href="mailto:#">some.email@somewhere.com</a><br>
-						<br>
-						234 Hidden Pond Road, Ashland City, TN 37015
-					</p>	
-				</div>
-			</div>
-
-		</div> <!-- /row of widgets -->
-	</div>
-</footer>
-
 <footer id="underfooter">
 	<div class="container">
 		<div class="row">
-			
-			<div class="col-md-6 widget">
-				<div class="widget-body">
-					<p>234 Hidden Pond Road, Ashland City, TN 37015 </p>
-				</div>
-			</div>
 
-			<div class="col-md-6 widget">
+			<div class="col-md-12 widget">
 				<div class="widget-body">
-					<p class="text-right">
-						Copyright &copy; 2014, Your awesome name here<br> 
-						Design: <a href="http://www.gettemplate.com" rel="designer">Initio by GetTemplate</a> </p>
+					<p class="text-center">
+						Copyright &copy; 2018, Katerina Trajchevska</p>
 				</div>
 			</div>
 
--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,25 @@ Route::get('/', function () {
     $projects = [
         [
             'name' => "Adeva's Female Bootcamp",
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nobis quo ipsam repellat, illum similique quisquam, temporibus, nisi est eligendi dignissimos. Ullam itaque modi illo hic doloremque. Labore, eos, vero!',
-            'href' => '',
+            'description' => 'The Female Bootcamp is a non profitable initiative, aimed to raise awareness about the state of women in tech in Macedonia and empower 20 women to pursue a career in tech.',
+            'involvement' => 'Organizer and Mentor',
+            'href' => 'https://femalebootcamp.adevait.com/',
             'image' => 'female-bootcamp.png',
             'tags' => ['gender equality', 'women in tech'],
         ],[
             'name' => 'We Talk IT: Developers\' Community',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nobis quo ipsam repellat, illum similique quisquam, temporibus, nisi est eligendi dignissimos. Ullam itaque modi illo hic doloremque. Labore, eos, vero!',
-            'href' => '',
+            'description' => 'We Talk IT is a community for developers and tech enthusiasts based in Macedonia. Its goal is to encourage collaboration, promote knowledge sharing and contribute to the growth of the Macedonian tech ecosystem.',
+            'href' => 'https://wetalkit.xyz/',
+            'involvement' => 'Initiator and Community Leader',
             'image' => 'wetalkit.png',
-            'tags' => ['developers', 'community'],
+            'tags' => ['developers', 'community', 'volunteering'],
         ],[
             'name' => 'Learn IT, Girl',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nobis quo ipsam repellat, illum similique quisquam, temporibus, nisi est eligendi dignissimos. Ullam itaque modi illo hic doloremque. Labore, eos, vero!',
-            'href' => '',
+            'description' => 'Learn IT, Girl is a global initiative for improving the state of women in tech. The goal is to match women who think about starting a career in technology with mentors who could help them achieve it.',
+            'involvement' => 'Organizer and Mentor',
+            'href' => 'https://www.learnitgirl.com/',
             'image' => 'learnitgirl.png',
-            'tags' => ['gender equality', 'women in tech'],
+            'tags' => ['gender equality', 'women in tech', 'volunteering'],
         ],
     ];
 
@@ -59,30 +62,30 @@ Route::get('/', function () {
             'href' => 'https://adevait.com/blog/remote-work/remote-better-developer-communicating/',
             'image' => 'remote-communication.jpg',
             'tags' => ['remote work', 'developers'],
-        ],[
-            'name' => "This is what happens when you get to work from wherever you want",
-            'description' => 'At the office, Automatticians had an amazing environment, books, games, all the equipment they needed. Why did they choose remote instead?',
-            'href' => 'https://wetalkit.xyz/this-is-what-happens-when-you-get-to-work-from-wherever-you-want-e0d7fed0033a',
-            'image' => 'automattic.jpeg',
-            'tags' => ['remote work'],
-        ],[
-            'name' => "How to lead a remote team that will actually bring results",
-            'description' => 'What would you do if the only thing that mattered was how good your employees are and not where they’re located? Would you go beyond traditional hiring?',
-            'href' => 'https://medium.com/@trajchevska/how-to-lead-a-remote-team-that-will-actually-bring-results-4bb6862ac3b7',
-            'image' => 'remote-lead.jpeg',
-            'tags' => ['remote work', 'leadership'],
+        // ],[
+        //     'name' => "This is what happens when you get to work from wherever you want",
+        //     'description' => 'At the office, Automatticians had an amazing environment, books, games, all the equipment they needed. Why did they choose remote instead?',
+        //     'href' => 'https://wetalkit.xyz/this-is-what-happens-when-you-get-to-work-from-wherever-you-want-e0d7fed0033a',
+        //     'image' => 'automattic.jpeg',
+        //     'tags' => ['remote work'],
+        // ],[
+        //     'name' => "How to lead a remote team that will actually bring results",
+        //     'description' => 'What would you do if the only thing that mattered was how good your employees are and not where they’re located? Would you go beyond traditional hiring?',
+        //     'href' => 'https://medium.com/@trajchevska/how-to-lead-a-remote-team-that-will-actually-bring-results-4bb6862ac3b7',
+        //     'image' => 'remote-lead.jpeg',
+        //     'tags' => ['remote work', 'leadership'],
         ],[
             'name' => "Growth Lessons From Instagram, Before It Was Instagram",
             'description' => 'Instagram was built in 8 weeks, it went viral in a matter of hours. It looks so easy it’s unnerving. Well…not so fast. There is another side of the story.',
             'href' => 'https://thinkgrowth.org/growth-lessons-from-instagram-before-it-was-instagram-cf414612330b',
             'image' => 'instagram.jpeg',
             'tags' => ['startups', 'growth'],
-        ],[
-            'name' => "Introducing We Talk IT: the Developers' Community",
-            'description' => 'Welcome to We Talk IT: the tech community that empowers developers, encourages collaboration and making a change through technology.',
-            'href' => 'https://wetalkit.xyz/introducing-wetalkit-e6e74a4e44da',
-            'image' => 'wetalkit.png',
-            'tags' => ['community', 'developers'],
+        // ],[
+        //     'name' => "Introducing We Talk IT: the Developers' Community",
+        //     'description' => 'Welcome to We Talk IT: the tech community that empowers developers, encourages collaboration and making a change through technology.',
+        //     'href' => 'https://wetalkit.xyz/introducing-wetalkit-e6e74a4e44da',
+        //     'image' => 'wetalkit.png',
+        //     'tags' => ['community', 'developers'],
         ],[
             'name' => "Women in tech: constant fight for gender balance",
             'description' => 'Even in an era when gender inequality may seem like a problem from the past, things are not yet very balanced. And they are even more alarming in tech world.',
@@ -92,9 +95,29 @@ Route::get('/', function () {
         ],
     ];
 
+    $conferences = [
+        [
+            'name' => "Code Talks, Hamburg",
+            'description' => 'Speaking about good software design practices at the largest developers\' conference in Germany. The presentation aims to show how what can be a minor effort if done on time can cause major delays if not.',
+            'href' => 'https://www.codetalks.de/speakers#talk-241',
+        ],[
+            'name' => "Laracon EU, Amsterdam",
+            'description' => 'I was talking about SOLID design principles at the largest Laravel conference in Europe. I had the chance to share good design practices with an audience of 800 developers, which was an amazing experience.',
+            'href' => 'https://laracon.eu/2018/speakers/katerina/',
+        ],[
+            'name' => "DEVit, Thessaloniki",
+            'description' => 'I held a workshop on SOLID design principles and good code design at the leading web developer conference in South East Europe. It helped around 30 developers learn how to write software that\'s easy to extend and maintain.',
+            'href' => 'https://devitconf.org/speakers/katerina_trajchevska/',
+        ],[
+            'name' => "Fin Hackathon, Skopje",
+            'description' => 'I regularly take part as a mentor in local hackathons. The last one I joined was organized by Macedonian Ministry of Finance and aimed to find a simpler way to present the public budget. It was a great cause and an amazing experience.',
+            'href' => 'https://www.finance.gov.mk/mk/node/7334',
+        ],
+    ];
+
     $now = \Carbon\Carbon::now();
     $adevaFounded = \Carbon\Carbon::create('2015', '01', '01');
     $adevaLeadYears = $now->diffInYears($adevaFounded);
 
-    return view('index', compact('projects', 'insights', 'adevaLeadYears'));
+    return view('index', compact('projects', 'insights', 'conferences', 'adevaLeadYears'));
 });
